0b0449f1bd5ef1675561e46b8661a0945ec05575
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-06-09 01:40:26 -0300

    Finalizando toda estrutura do portfólio

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.animacao', {
+    delay: 300,
+    distance: '20px',
+    origin: 'bottom',
+    duration: 1000,
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    reset: true 
+})
+
