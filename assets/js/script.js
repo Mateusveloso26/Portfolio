c0ce9579b88372d9dceb0508d5ab58fc38833fe4
Mateus Fernandes <mateusveloso2608@gmail.com>
2024-07-12 10:59:20 -0300

    menu responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ ScrollReveal().reveal('.animacao', {
     easing: 'cubic-bezier(0.5, 0, 0, 1)',
     reset: true
 })
+
+function abrirMenu() {
+    document.getElementById('menuNav').style.width = '100%';
+}
+
+function fecharMenu() {
+    document.getElementById('menuNav').style.width = '0';
+}
